8bdf64a7312f86f1b7c3e78a24d5f28ac97cf265
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-01-25 15:35:31 +0330

    Update ALU.v

--- MIPS/ALU.v
+++ MIPS/ALU.v
@@ -19,40 +19,93 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ALU(
-    input wire [3:0] ALUCnt,
+    input wire [4:0] ALUCnt,
     input wire [31:0] input1,
     input wire [31:0] input2,
     input [4:0] shamt,
     output reg [31:0] result,
     output reg zero
     );
+initial begin
+	result = 0;
+	zero = 1;
+end
+	 
 always @(*) begin
-if(ALUCnt == 0) begin
-	result = input1 + input2;
-end
-if(ALUCnt == 1) begin
-	result = input1 - input2;
-end
-if(ALUCnt == 2) begin
-	result = ~input1;
-end
-if(ALUCnt == 3) begin
-	result = input1 << shamt;
-end
-if(ALUCnt == 4) begin
-	result = input1 >> shamt;
-end
-if(ALUCnt == 5) begin
-	result = input1 & input2;
-end
-if(ALUCnt == 6) begin
-	result = input1 | input2;
-end
-if(ALUCnt == 7) begin
-	result = (input1 < input2)? 1: 0;
-end
+	if(ALUCnt == 0) begin
+		result = input1 + input2;
+		zero = result? 0:1;
+	end
+	else if(ALUCnt == 1) begin
+		result = input1 - input2;
+		zero = result? 0:1;
+	end
+	else if(ALUCnt == 2) begin
+		result = ~input1;
+		zero = result? 0:1;
+	end
+	else if(ALUCnt == 3) begin
+		result = input1 << shamt;
+		zero = result? 0:1;
+	end
+	else if(ALUCnt == 4) begin
+		result = input1 >> shamt;
+		zero = result? 0:1;
+	end
+	else if(ALUCnt == 5) begin
+		result = input1 & input2;
+		zero = result? 0:1;
+	end
+	else if(ALUCnt == 6) begin
+		result = input1 | input2;
+		zero = result? 0:1;
+	end
+	else if(ALUCnt == 7) begin
+		result = (input1 < input2)? 1: 0;
+		zero = result? 0:1;
+	end
 
-zero = result? 0:1;
+	//------------------------------------------------
+	else if(ALUCnt == 8) begin // beq
+		zero = (input1 == input2)? 1: 0;
+	end
+	else if(ALUCnt == 9) begin // bne
+		zero = (input1 != input2)? 1: 0;
+	end
+	else if(ALUCnt == 10) begin // bgt
+		zero = (input1 > input2)? 1: 0;
+	end
+	else if(ALUCnt == 11) begin // bge
+		zero = (input1 >= input2)? 1: 0;
+	end
+	else if(ALUCnt == 12) begin // blt
+		zero = (input1 < input2)? 1: 0;
+	end
+	else if(ALUCnt == 13) begin // ble
+		zero = (input1 <= input2)? 1: 0;
+	end
+	//------------------------------------------------	
+	else if(ALUCnt == 14) begin // jump
+		//result = inpu1 + input2;
+		zero = 1;
+	end
+	//------------------------------------------------
+	else if(ALUCnt == 15) begin // imul
+		result = input1 * input2;
+		zero = result? 0:1;
+	end
+	//------------------------------------------------
+	else if(ALUCnt == 16) begin // divi
+		result = input1 / input2;
+		zero = result? 0:1;
+	end
+	//------------------------------------------------
+	else if(ALUCnt == 17) begin // sra
+		result = input1 >>> input2;
+		zero = result? 0:1;
+	end
+	//------------------------------------------------
+
 end
 
 endmodule

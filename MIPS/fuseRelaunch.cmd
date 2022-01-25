ca765cddb3f84e80ca528b047eb8dc5bf33ed965
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-01-25 15:43:11 +0330

    Add Files

---
+++ MIPS/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/emir/homework/ProgramCounter/MIPS_isim_beh.exe" -prj "C:/Users/emir/homework/ProgramCounter/MIPS_beh.prj" "work.MIPS" "work.glbl" 
